0c788059087df783ae16dcd7da66512f41d2bf2d
Gautam <gautamgok9018@gmail.com>
2024-11-17 02:26:38 +0530

    Initial changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ const App = () => {
   return (
     <Router>
       <div>
-        <h1>Welcome to My App</h1>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
